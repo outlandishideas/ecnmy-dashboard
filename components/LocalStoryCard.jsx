35f4a7b9f27d16fbd81ecee3e17a7f6f1c32e537
Noel Light-Hilary <noel@noellh.com>
2022-09-09 16:59:44 +0100

    ECONOMY-33 – local story blurb

--- components/LocalStoryCard.jsx
+++ components/LocalStoryCard.jsx
@@ -15,8 +15,7 @@ export default function LocalStoryCard() {
         <ul className="list-disc m-4 text-base"></ul>
         <section className="flex justify-between items-center">
           <div className="flex items-center">
-            Find out more about how to access a network of contributors who can
-            share their lived experience of economic issues in your local area.
+            Access our network of contributors who can share their lived experience of this economic issue in this local area.
           </div>
         </section>
         <section className="w-full">
